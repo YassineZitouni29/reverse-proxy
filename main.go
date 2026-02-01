2a189e44b0130dad4bcf253e75f7e012714d620c
YassineZitouni29 <yassine.zitouni@um6p.ma>
2026-02-01 22:28:14 +0100

    I added the part responsible for launching the admin server

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"reverse-proxy/admin"
 	"reverse-proxy/serverPool"
 	"strings"
 	"syscall"
@@ -17,26 +18,24 @@ import (
 )
 
 type ProxyConfig struct {
-	Port            string        `json:"proxyPort"`
-	AdminPort       string        `json:"adminPort"`
-	Strategy        string        `json:"strategy"`
-	HealthCheckFreq string        `json:"healthCheckFreq"`
-	Urls            []string      `json:"urls"`
+	Port            string   `json:"proxyPort"`
+	AdminPort       string   `json:"adminPort"`
+	Strategy        string   `json:"strategy"`
+	HealthCheckFreq string   `json:"healthCheckFreq"`
+	Urls            []string `json:"urls"`
 }
-
-
 
 func main() {
 	pool := &serverpool.ServerPool{}
 	var config ProxyConfig
 
-	file, err:= os.ReadFile("config.json")
+	file, err := os.ReadFile("config.json")
 
-	if err!= nil{
+	if err != nil {
 		panic("Cannot decode the urls")
 	}
 
-	if err = json.Unmarshal(file, &config); err != nil{
+	if err = json.Unmarshal(file, &config); err != nil {
 		panic("Cannot decode config file")
 	}
 
@@ -45,9 +44,9 @@ func main() {
 		strategy = "round-robin"
 	}
 
-	for _, rawURL := range config.Urls{
+	for _, rawURL := range config.Urls {
 		urll, _ := url.Parse(rawURL)
-		backend:= serverpool.Backend{URL: urll, Alive: false}
+		backend := serverpool.Backend{URL: urll, Alive: false}
 		pool.AddBackend(&backend)
 	}
 
@@ -59,45 +58,45 @@ func main() {
 	type backendKey struct{}
 
 	proxy := httputil.ReverseProxy{
-			Director: func(r *http.Request){
-					next_backend, _ := r.Context().Value(backendKey{}).(*serverpool.Backend)
-					if next_backend == nil {
-						next_backend = pool.GetNextValidPeer(strategy)
-					}
-				if next_backend==nil{
-					r.URL = &url.URL{}
-					r.Host = ""
-					return
+		Director: func(r *http.Request) {
+			next_backend, _ := r.Context().Value(backendKey{}).(*serverpool.Backend)
+			if next_backend == nil {
+				next_backend = pool.GetNextValidPeer(strategy)
+			}
+			if next_backend == nil {
+				r.URL = &url.URL{}
+				r.Host = ""
+				return
+			}
+			next_backend.CurrentConns.Add(1)
+			fmt.Println("forwarded to ", next_backend.URL)
+
+			ctx := context.WithValue(r.Context(), backendKey{}, next_backend)
+			*r = *r.WithContext(ctx)
+
+			r.URL.Scheme = next_backend.URL.Scheme
+			r.URL.Host = next_backend.URL.Host
+			r.Host = next_backend.URL.Host
+		},
+		ModifyResponse: func(r *http.Response) error {
+			if b, ok := r.Request.Context().Value(backendKey{}).(*serverpool.Backend); ok {
+				b.CurrentConns.Add(^int64(0))
+
+			}
+			return nil
+		},
+		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
+			if b, ok := r.Context().Value(backendKey{}).(*serverpool.Backend); ok {
+				b.CurrentConns.Add(^int64(0))
+
+				if errors.Is(err, syscall.ECONNREFUSED) {
+					b.SetAlive(false)
 				}
-				next_backend.CurrentConns.Add(1)
-				fmt.Println("forwarded to ", next_backend.URL)
-
-				ctx:= context.WithValue(r.Context(),backendKey{}, next_backend)
-				*r = *r.WithContext(ctx)
-
-				r.URL.Scheme = next_backend.URL.Scheme
-				r.URL.Host = next_backend.URL.Host
-				r.Host = next_backend.URL.Host
-			},
-			ModifyResponse: func(r *http.Response) error{
-				if b, ok := r.Request.Context().Value(backendKey{}).(*serverpool.Backend); ok {
-					b.CurrentConns.Add(^int64(0))
-
-				}
-				return nil
-			},
-			ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
-				if b, ok:= r.Context().Value(backendKey{}).(*serverpool.Backend); ok{
-					b.CurrentConns.Add(^int64(0))
-
-					if errors.Is(err, syscall.ECONNREFUSED) {
-						b.SetAlive(false)
-					}
-				} 
-				log.Printf("proxy error: %v", err)
-				http.Error(w, "no backends available", http.StatusServiceUnavailable)
-			},
-		}
+			}
+			log.Printf("proxy error: %v", err)
+			http.Error(w, "no backends available", http.StatusServiceUnavailable)
+		},
+	}
 
 	http.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
 		backend := pool.GetNextValidPeer(strategy)
@@ -114,21 +113,26 @@ func main() {
 		healthFreq = 5 * time.Millisecond
 	}
 
-	go func(){
-		ticker:= time.NewTicker(healthFreq)
+	go func() {
+		ticker := time.NewTicker(healthFreq)
 		defer ticker.Stop()
-		for range ticker.C{
+		for range ticker.C {
 			pool.CheckHealth()
 		}
 	}()
-	
+
+	adminPort := config.AdminPort
+	if adminPort == "" {
+		adminPort = "8081"
+	}
+	admin.Start(pool, adminPort)
 
 	port := config.Port
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Println("Reverse proxy listening on :"+port)
+	log.Println("Reverse proxy listening on :" + port)
 	http.ListenAndServe(":"+port, nil)
 
 }
